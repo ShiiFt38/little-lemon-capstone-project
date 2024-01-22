402f1770b661b7d029612f2943cde60aff86ab64
ShiiFt38 <maseemet0@gmail.com>
2024-01-22 11:02:42 +0200

    basic components

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import Nav from './Nav'
+import Logo from '../assets/Logo .svg'
+
+const Header = () => {
+
+    return(
+    <header>
+        <img alt="lemon logo" src={Logo}></img>
+        <Nav/>
+    </header>
+    );
+};
+
+export default Header
---
+++ src/components/Main.js
@@ -0,0 +1,9 @@
+const Main = () => {
+    return (
+        <main id="main">
+        <h1>Main Here</h1>
+        </main>
+    );
+};
+
+export default Main
